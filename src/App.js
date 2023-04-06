3073f45941d26f9fda25e4375356168012f4cc13
mohammaddarweesh-oop <githubdarweesh@gmail.com>
2023-04-07 00:27:01 +0300

    commit 6
    execute style css for swiper

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import SwiperCrousal from "./Components/SwiperCrousal/SwiperCrousal";
 
 function App() {
   // https://swiperjs.com/demos
-  // let userData = [
-  //   { id: 1, name: "Jehad", email: "mohammad@gmail.com", age: 24 },
-  //   { id: 2, name: "Ahmad", email: "ahmad@gmail.com", age: 22 },
-  //   { id: 3, name: "Osama", email: "osama@gmail.com", age: 44 },
-  //   { id: 4, name: "Nader", email: "nader@gmail.com", age: 30 },
-  //   { id: 5, name: "Same", email: "same@gmail.com", age: 33 },
-  //   { id: 6, name: "Khaled", email: "khaled@gmail.com", age: 70 },
-  //   { id: 5, name: "Faiza", email: "faiza@gmail.com", age: 23 },
-  //   { id: 5, name: "Dawood", email: "dawood@gmail.com", age: 14 },
-  //   { id: 5, name: "Moaath", email: "moaath@gmail.com", age: 18 },
-  //   { id: 5, name: "Sulaiman", email: "Sulaiman@gmail.com", age: 24 },
-  // ];
+  let userData = [
+    { id: 1, name: "Jehad", email: "mohammad@gmail.com", age: 24 },
+    { id: 2, name: "Ahmad", email: "ahmad@gmail.com", age: 22 },
+    { id: 3, name: "Osama", email: "osama@gmail.com", age: 44 },
+    { id: 4, name: "Nader", email: "nader@gmail.com", age: 30 },
+    { id: 5, name: "Same", email: "same@gmail.com", age: 33 },
+    { id: 6, name: "Khaled", email: "khaled@gmail.com", age: 70 },
+    { id: 5, name: "Faiza", email: "faiza@gmail.com", age: 23 },
+    { id: 5, name: "Dawood", email: "dawood@gmail.com", age: 14 },
+    { id: 5, name: "Moaath", email: "moaath@gmail.com", age: 18 },
+    { id: 5, name: "Sulaiman", email: "Sulaiman@gmail.com", age: 24 },
+  ];
 
-  // const view = () => {
+  return (
+    <div className="App">
+      <SwiperCrousal />
+    </div>
+  );
+}
+
+export default App;
+
+/**
+ * 
+ * 
+ * // const view = () => {
   //   const ss = userData.map((el) => (
   //     <SwiperSlide>
   //       <div>
@@ -50,16 +62,6 @@ function App() {
   //     spaceBetween: 40,
   //   },
   // };
-  return (
-    <div className="App">
-      <SwiperCrousal />
-    </div>
-  );
-}
-
-export default App;
-
-/**
  * 
  * 
  * 
--- src/Components/SwiperCrousal/SwiperCrousal.js
+++ src/Components/SwiperCrousal/SwiperCrousal.js
@@ -4,25 +4,19 @@ import SwiperCore, { Pagination, Navigation } from "swiper/core";
 import "swiper/swiper.min.css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "./Swiper.css";
+import "./Style.css";
 
 SwiperCore.use([Pagination, Navigation]);
 
 const SwiperCrousal = () => {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={2}
-      navigation
-      pagination
-      className="swiper"
-    >
-      <SwiperSlide className="swiper-slide">Mohammad</SwiperSlide>
-      <SwiperSlide className="swiper-slide">Slide 2</SwiperSlide>
-      <SwiperSlide className="swiper-slide">Slide 3</SwiperSlide>
-      <SwiperSlide className="swiper-slide">Slide 4</SwiperSlide>
-      <SwiperSlide className="swiper-slide">Mohammad</SwiperSlide>
-      <SwiperSlide className="swiper-slide">Slide 2</SwiperSlide>
+    <Swiper spaceBetween={50} slidesPerView={2} navigation pagination>
+      <SwiperSlide>Mohammad</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Mohammad</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
     </Swiper>
   );
 };
