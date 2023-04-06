bf25ed01d46ae82022a278692365a22edacb5560
mohammaddarweesh-oop <githubdarweesh@gmail.com>
2023-04-06 16:10:52 +0300

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      MOHAMMAD
+      <h1>ENG Mohammad Darweesh</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur 
+      adipisicing elit. Maxime, sequi quos. Laborum 
+      voluptatibus ipsa sit repudiandae neque facere 
+      quasi qui veritatis 
+      earum. Soluta illum maxime magni asperiores aut facere nihil.</p>
     </div>
   );
 }
